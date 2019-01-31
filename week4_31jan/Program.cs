d6cb0f03a4ee990150ad8aec9a40752a7fdb3df0
visualstudio <visualstudio@230-PC13>
2019-01-31 16:54:44 -0500

    week4

--- week4_31jan/Program.cs
+++ week4_31jan/Program.cs
@@ -12,66 +12,97 @@ namespace week4_31jan
         {
             Countryside Ontario = new Countryside();
             Ontario.run();
+
         }
-        class Village
+    }
+
+    class LearningExample
+    {
+        public void run()
         {
-            public static int numberofvillages = 0;
-            public Village nextvillage;
-            public Village previousvillage;
-            public string Villagename;
-            public bool isAstrildeHere = false;
+            Village Toronto;
+            Village a;
+            Village b;
+            Toronto = new Village();
+            a = Toronto;
 
-            public Village()
-            { Village.numberofvillages++; }
+            Toronto = new Village();
+            b = Toronto;
+
+            if (a == b) { Console.WriteLine("same"); }
+            else
+            { Console.WriteLine("different obj refs"); }
         }
-        class Countryside
+    }
+    class Village
+    {
+        public static int numberOfVillages = 0;
+        public Village nextVillage;
+        public Village previousVillage;
+        public string VillageName;
+        public bool isAstrildeHere = false;
+
+        public Village()
         {
-            public Village Maple;
+            Village.numberOfVillages++;
+        }
+    }
 
-            public Village Toronto;
-            public Village Ajax;
-            public Village Head;
-            public Village Tail;
-            public Village Temp;
+    class Countryside
+    {
+        public Village Maple;
+        public Village Toronto;
+        public Village Ajax;
+        public Village Head;
+        public Village Tail;
+        public Village Current;
 
-            public void run()
+        public void run()
+        {
+            this.MapInitializer();
+            this.LookForAstrilde();
+            Console.WriteLine("Hugi found Astrilde in " + Current.VillageName);
+        }
+
+        public void MapInitializer()
+        {
+            Ajax = new Village();
+            Toronto = new Village();
+            Maple = new Village();
+
+            Maple.VillageName = "Maple";
+            Maple.previousVillage = null;
+            Maple.nextVillage = Toronto;
+            Maple.isAstrildeHere = true;
+
+            Toronto.previousVillage = Maple;
+            Toronto.VillageName = "Toronto";
+            Toronto.nextVillage = Ajax;
+
+            Ajax.VillageName = "Ajax";
+            Ajax.nextVillage = null;
+            Ajax.previousVillage = Toronto;
+
+        }
+        public void LookForAstrilde()
+        {
+
+            Current = Maple;
+            while (Current.nextVillage != null)
             {
-                this.MapInitializer();
-                this.LookForAstrilde();
-            }
-            public void MapInitializer()
-            {
-                Maple = new Village();
-                Maple.Villagename = "Maple";
-                Maple.previousvillage = null;
-                Maple.nextvillage = Toronto;
-                Maple.isAstrildeHere = true;
-
-                Toronto = new Village();
-                Toronto.Villagename = "Toronto";
-                Toronto.previousvillage = null;
-                Ajax = new Village();
-                Ajax.Villagename = "Ajax";
-                Ajax.nextvillage = null;
-                Ajax.previousvillage = Toronto;
-                // Ajax.isAstrildeHere = true;
-
-
-            }
-
-            public void LookForAstrilde()
-            {
-                Head = Maple;
-                if (Head.isAstrildeHere)
+                if (Current.isAstrildeHere)
                 {
-                    Console.WriteLine("yeah astridle is in " + Head.Villagename);
+                    Console.WriteLine("Found Astrilde");
+                    return;
+
                 }
-
-               // while (true)
+                else
                 {
+                    Current = Current.nextVillage;
 
                 }
             }
         }
+
     }
 }
